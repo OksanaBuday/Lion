17d92aaab5184ff8d68c0313d41b8bab829e5f31
oksanabuday <oksanabuday@gmail.com>
2017-01-23 22:05:52 +0200

    Initial commit

---
+++ src/FF.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Oksana on 22.01.2017.
+ */
+public class FF {
+    public static void main(String[] args) {
+        String myArray [] = { "one", "two", "tree" };
+        for (String i : myArray){
+            System.out.println("i =" +i);
+            break;
+        }
+    }
+}
